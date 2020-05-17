2736cf3d814556505c2cdd48c6d91e7d24c34451
bmlindelwa <3441209@myuwc.ac.za>
2020-05-17 22:10:20 +0200

    Updates

---
+++ precache-manifest.f73d1f511f699c2ca6543f2c4ccc3440.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "656afeb456e6f888bcd65045bedc3d83",
+    "url": "/sisakazi/index.html"
+  },
+  {
+    "revision": "911ed5d9918d60d55a8e",
+    "url": "/sisakazi/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "6e5187f41878ee9300ab",
+    "url": "/sisakazi/static/css/main.96b93b89.chunk.css"
+  },
+  {
+    "revision": "911ed5d9918d60d55a8e",
+    "url": "/sisakazi/static/js/2.fb329bdf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sisakazi/static/js/2.fb329bdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e5187f41878ee9300ab",
+    "url": "/sisakazi/static/js/main.c9a0a32e.chunk.js"
+  },
+  {
+    "revision": "cea00535af67e17792b6",
+    "url": "/sisakazi/static/js/runtime-main.0e762396.js"
+  },
+  {
+    "revision": "860d90c0a894b0a287638f6e2a4d5e09",
+    "url": "/sisakazi/static/media/283359.860d90c0.jpg"
+  },
+  {
+    "revision": "65f7049bfa7fd69ffa2c0b05b8fcb011",
+    "url": "/sisakazi/static/media/african-dress.65f7049b.jpg"
+  },
+  {
+    "revision": "664b88bc137ffe6ce03c7ef95710e462",
+    "url": "/sisakazi/static/media/beaded-double-necklace.664b88bc.jpg"
+  },
+  {
+    "revision": "72b7f8b0237c50d3f327dc1c97e0e00b",
+    "url": "/sisakazi/static/media/chain-puffed-bag.72b7f8b0.jpg"
+  },
+  {
+    "revision": "4a6a871d9b5e56af4502e9e477267ee6",
+    "url": "/sisakazi/static/media/charlotte-top.4a6a871d.jpg"
+  },
+  {
+    "revision": "4495522879203d1d530635d670f795e5",
+    "url": "/sisakazi/static/media/cross-body-bag.44955228.jpg"
+  },
+  {
+    "revision": "6e9ce94f0454404722ee457a34cb57d3",
+    "url": "/sisakazi/static/media/formal-midi-dress.6e9ce94f.jpg"
+  },
+  {
+    "revision": "d4db397868694a7a5c17c90beb89fe61",
+    "url": "/sisakazi/static/media/gina-blazer-dress.d4db3978.jpg"
+  },
+  {
+    "revision": "f73dec3c46accd9e939676a2a17cb01a",
+    "url": "/sisakazi/static/media/girls-summer.f73dec3c.jpg"
+  },
+  {
+    "revision": "9daf083f8e36ebff8f693ea12179cd55",
+    "url": "/sisakazi/static/media/heeled-sandals.9daf083f.jpg"
+  },
+  {
+    "revision": "9a9d79cc1ab232abff6d44ac96de9498",
+    "url": "/sisakazi/static/media/jogger-pants.9a9d79cc.jpg"
+  },
+  {
+    "revision": "43c1816182aabd8978d6b6eba8c680b5",
+    "url": "/sisakazi/static/media/mannequin.43c18161.jpg"
+  },
+  {
+    "revision": "890281dd1ce033381e74536a0fc0f1e7",
+    "url": "/sisakazi/static/media/mid-heel-sandal.890281dd.jpg"
+  },
+  {
+    "revision": "e01bf69cc8dee39ca7cb69f7e536a4b6",
+    "url": "/sisakazi/static/media/pink-colorblock.e01bf69c.jpg"
+  },
+  {
+    "revision": "b72446538217cab713c4ccbe75af1d43",
+    "url": "/sisakazi/static/media/pink-effect-jacket.b7244653.jpg"
+  },
+  {
+    "revision": "f703447808fa143e4749f87308988100",
+    "url": "/sisakazi/static/media/river-island.f7034478.jpg"
+  },
+  {
+    "revision": "8e3d11b92bae8128af3f2b7e457244a0",
+    "url": "/sisakazi/static/media/summer-long-dress.8e3d11b9.jpg"
+  },
+  {
+    "revision": "30577ff39a3a1ffd02f62fa82d6c8562",
+    "url": "/sisakazi/static/media/three-hoop-earrings.30577ff3.jpg"
+  },
+  {
+    "revision": "f6565269dc5f11878e2f636dfd1f7e1a",
+    "url": "/sisakazi/static/media/womens-summer.f6565269.jpg"
+  }
+]);
